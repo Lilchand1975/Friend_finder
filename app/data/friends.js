43b23c6090e6645848d4df630ed214784d2e461d
chandra Chhetri <chhetricc@gmail.com>
2018-08-04 17:33:21 -0400

    committing repo find friend

---
+++ app/data/friends.js
@@ -0,0 +1,486 @@
+var friends = [
+    {
+    "name": "Binay",
+    "photo": "https: //www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj2r7ui8srcAhXQo1kKHUV4BQwQjRx6BAgBEAU&url=https%3A%2F%2Flilpickmeup.com%2F2015%2F07%2F20%2Fthe-value-of-forming-organic-friendships%2F&psig=AOvVaw2q5rLQQiTCJqyyXIuM3ses&ust=1533179853202304jpg",
+
+    "scores":[
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Haroon Deming",
+    "photo": "https: //www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjAwv3V9MrcAhXFslkKHWE7CngQjRx6BAgBEAU&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DaC3GoTIjCq4&psig=AOvVaw20lhiVlBelsNgPtRgWFoSd&ust=1533180478017618",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "Jeremiah Falcon",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwibqNjT9crcAhXQrVkKHRfzAMUQjRx6BAgBEAU&url=http%3A%2F%2Fwww.technocrazed.com%2F20-sweet-examples-of-friendship-between-animals-of-different-species-photo-gallery&psig=AOvVaw20lhiVlBelsNgPtRgWFoSd&ust=1533180478017618",
+    "scores": [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Markus M. Pelia",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjsp6jp9crcAhXPxlkKHWeWAiMQjRx6BAgBEAU&url=http%3A%2F%2Fwww.earthporm.com%2F23-unusual-animal-friendships-absolutely-adorable%2F&psig=AOvVaw20lhiVlBelsNgPtRgWFoSd&ust=1533180478017618",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Leo Bitter",
+    "photo":"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjUq9X49crcAhVvqlkKHUUsCXwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fvegnews%2Funlikely-animal-friends%2F&psig=AOvVaw20lhiVlBelsNgPtRgWFoSd&ust=1533180478017618",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "dan Bison",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjsp7ah9srcAhUPm1kKHZeyCQwQjRx6BAgBEAU&url=http%3A%2F%2Foddstuffmagazine.com%2Flife-is-beautiful-when-you-go-over-it-with-your-friends.html&psig=AOvVaw20lhiVlBelsNgPtRgWFoSd&ust=1533180478017618",
+    "scores": [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+    {
+    "name": "lulia",
+    "photo": "https://www.google.com/imgres?imgurl=http://www.gstatic.com/tv/thumb/persons/378/378_v9_bd.jpg&imgrefurl=http://google.com/search?tbm%3Disch%26q%3DTom%2520Cruise&h=1440&w=1080&tbnid=BQsiWgZFHtA-xM:&q=tom+cruise&tbnh=186&tbnw=139&usg=__PSWK7JCjqWjFJV-Q3eUL0HIuKI4%3D&vet=12ahUKEwjg-cPJgsrcAhUJbKwKHbjHCz8Q_B0wLXoECA8QCQ..i&docid=CQuuqeZylkHWkM&itg=1&sa=X&ved=2ahUKEwjg-cPJgsrcAhUJbKwKHbjHCz8Q_B0wLXoECA8QCQ",
+    "scores": [
+    "2",
+    "1",
+    "1",
+    "2",
+    "2",
+    "3",
+    "2",
+    "2",
+    "5",
+    "3"
+    ]
+    },
+    {
+    "name": "syrion Bannister",
+    "photo": "https://vignette.wikia.nocookie.net/gameofthrones/images/5/58/Tyrion_main_s7_e6.jpg/revision/latest?cb=20170818050344",
+    "scores": [
+    "4",
+    "2",
+    "4",
+    "2",
+    "3",
+    "2",
+    "4",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Jiwan Lannister",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiHtZbL9srcAhWGt1kKHY6-ATAQjRx6BAgBEAU&url=https%3A%2F%2Ftwitter.com%2Ferinruberry%2Fstatus%2F861753715103064064&psig=AOvVaw20lhiVlBelsNgPtRgWFoSd&ust=1533180478017618",
+    "scores": [
+    "4",
+    "2",
+    "4",
+    "2",
+    "3",
+    "2",
+    "4",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Thinley Lannister",
+    "photo": "https://vignette.wikia.nocookie.net/gameofthrones/images/5/58/Tyrion_main_s7_e6.jpg/revision/latest?cb=20170818050344",
+    "scores": [
+    "4",
+    "2",
+    "4",
+    "2",
+    "3",
+    "2",
+    "4",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Andrew Hopkins",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjytNHxlsrcAhWFJHwKHYnSDjEQjRx6BAgBEAU&url=http%3A%2F%2Fs7.zetaboards.com%2FSodb%2Findex%2F&psig=AOvVaw1EeSMK4wJ014bdD8lJCpyJ&ust=1533155345388016",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "Bob Marley",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjf8eXd9srcAhWMslkKHYGICoEQjRx6BAgBEAU&url=http%3A%2F%2Fwww.golfian.com%2Fa-quarrel-between-friendswhen-made-up-adds-a-new-tie-to-friendship-saint-francis-de-sales-animals-quotes%2F&psig=AOvVaw20lhiVlBelsNgPtRgWFoSd&ust=1533180478017618",
+    "scores": [
+    "5",
+    "3",
+    "3",
+    "1",
+    "3",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4"
+    ]
+    },
+    {
+    "name": "Margreta Twin",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjMq9Hy9srcAhUJvVkKHWhBDPwQjRx6BAgBEAU&url=https%3A%2F%2Fgifer.com%2Fen%2FXijr&psig=AOvVaw20lhiVlBelsNgPtRgWFoSd&ust=1533180478017618",
+    "scores": [
+    "4",
+    "2",
+    "3",
+    "1",
+    "1",
+    "1",
+    "3",
+    "1",
+    "2",
+    "1"
+    ]
+    },
+    {
+    "name": "Simon Twain",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjNrNjJ98rcAhWlslkKHeZ5ADwQjRx6BAgBEAU&url=http%3A%2F%2Fwww.scoopwhoop.com%2Fentertainment%2Fblockbuster-friendship%2F&psig=AOvVaw3ZXJRBbtscRfSe38FuZ276&ust=1533181240529835",
+    "scores": [
+    "4",
+    "2",
+    "3",
+    "1",
+    "1",
+    "1",
+    "3",
+    "1",
+    "2",
+    "1"
+    ]
+    },
+    {
+    "name": "Dinesh Twain",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjt0qKb98rcAhUNjlkKHZEmCgsQjRx6BAgBEAU&url=https%3A%2F%2Fwww.ibtimes.co.in%2Ffriendship-day-unusual-bond-between-animals-photos-605832&psig=AOvVaw20lhiVlBelsNgPtRgWFoSd&ust=1533180478017618",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "Jacob E lee",
+    "photo": "google.com",
+    "scores": [
+    "2",
+    "3",
+    "4",
+    "3",
+    "2",
+    "4",
+    "5",
+    "5",
+    "5",
+    "4"
+    ]
+    },
+    {
+    "name": "Scott Parish",
+    "photo": "https://github.com/Soltis13",
+    "scores": [
+    "5",
+    "5",
+    "1",
+    "1",
+    "5",
+    "5",
+    "5",
+    "3",
+    "5",
+    "1"
+    ]
+    },
+    {
+    "name": "rott Brings",
+    "photo": "https://github.com/Soltis13",
+    "scores": [
+    "5",
+    "5",
+    "1",
+    "1",
+    "5",
+    "5",
+    "3",
+    "3",
+    "2",
+    "4"
+    ]
+    },
+    {
+    "name": "Scott Soltis",
+    "photo": "https://www.linkedin.com/in/jagnoor/",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "5",
+    "1",
+    "5",
+    "3"
+    ]
+    },
+    {
+    "name": "saibot Soltis",
+    "photo": "https://www.linkedin.com/in/jagnoor/",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "5",
+    "1",
+    "5",
+    "3"
+    ]
+    },
+    {
+    "name": "Prem Solta",
+    "photo": "https://www.linkedin.com/in/jagnoor/",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "5",
+    "1",
+    "5",
+    "3"
+    ]
+    },
+    {
+    "name": "Gary Pincer",
+    "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fd2gg9evh47fn9z.cloudfront.net%2F800px_COLOURBOX4560591.jpg&imgrefurl=https%3A%2F%2Fwww.colourbox.com%2Fimage%2Fangry-businessman-with-bat-on-white-image-4560591&docid=ZYFYh6oMlLoCsM&tbnid=RIFRAFdllEHXpM%3A&vet=10ahUKEwiXzM6hvsrcAhWBm4MKHWBjA3YQMwisASgAMAA..i&w=800&h=530&safe=off&bih=861&biw=1745&q=stock%20photo&ved=0ahUKEwiXzM6hvsrcAhWBm4MKHWBjA3YQMwisASgAMAA&iact=mrc&uact=8",
+    "scores": [
+    "4",
+    "4",
+    "4",
+    "1",
+    "3",
+    "1",
+    "4",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+    {
+    "name": "Ashley Thomas",
+    "photo": " Infinity Direct",
+    "scores": [
+    "5",
+    "3",
+    "4",
+    "1",
+    "3",
+    "1",
+    "3",
+    "1",
+    "4",
+    "4"
+    ]
+    },
+    {
+    "name": "Test Trinity",
+    "photo": "https://uat.noted.co.nz/media/12610/ns1117social-studiesgettyimages-532097678.jpg",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "3",
+    "1",
+    "3",
+    "2",
+    "5",
+    "4"
+    ]
+    },
+    {
+    "name": "Timothy Smith",
+    "photo": "~/Pictures/ggb_city_1280.jpg",
+    "scores": [
+    "4",
+    "5",
+    "4",
+    "2",
+    "4",
+    "2",
+    "5",
+    "2",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "arjun Pandit",
+    "photo": "dsa",
+    "scores": [
+    "3",
+    "2",
+    "3",
+    "5",
+    "1",
+    "3",
+    "3",
+    "4",
+    "1",
+    "3"
+    ]
+    },
+    {
+    "name": "raulabear valoo",
+    "photo": "https://www.mariowiki.com/images/thumb/f/f1/Toadette_-_Mario_Party_10.png/220px-Toadette_-_Mario_Party_10.png",
+    "scores": [
+    "3",
+    "3",
+    "1",
+    "4",
+    "4",
+    "3",
+    "2",
+    "1",
+    "4",
+    "5"
+    ]
+    },
+    {
+    "name": "Abhinav Regmi",
+    "photo": "https://goo.gl/images/wNw8QK",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "2",
+    "4",
+    "2",
+    "3",
+    "3",
+    "4",
+    "2"
+    ]
+    },
+    {
+    "name": "Aditi Chopra",
+    "photo": "https://goo.gl/images/wNw8QK",
+    "scores": [
+    "3",
+    "4",
+    "2",
+    "4",
+    "3",
+    "2",
+    "2",
+    "2",
+    "4",
+    "3"
+    ]
+    },
+    {
+    "name": "Melissa Manandhar",
+    "photo": "fsadf",
+    "scores": [
+    "3",
+    "2",
+    "1",
+    "4",
+    "3",
+    "2",
+    "5",
+    "3",
+    "5",
+    "3"
+    ]
+    }
+];
+
+module.exports = friends; 
+
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+//Dependencies/ creating file path to the html file
+var path = require("path");
+
+// ROUTING
+module.exports = function(app) {
+  // HTML GET Requests 
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+};
